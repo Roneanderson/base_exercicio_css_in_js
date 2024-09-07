714a15c1909c82642f8536b592d6518135006f38
Rone <roneanderson469@gmail.com>
2024-09-06 22:36:40 -0300

    add perfil

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,18 +1,9 @@
-import styled from 'styled-components'
+import { Perfil } from './Perfil.styles'
 
-const Perfil = styled.header`
-  background-color: var(--cor-secundaria);
-  color: var(--cor-principal);
-  text-align: center;
-  padding: 24px 0;
-`
-
-function Cabecalho() {
-  return (
-    <Perfil>
-      <h1>EBAC Jobs</h1>
-    </Perfil>
-  )
-}
+const Cabecalho = () => (
+  <Perfil>
+    <h1>EBAC Jobs</h1>
+  </Perfil>
+)
 
 export default Cabecalho
